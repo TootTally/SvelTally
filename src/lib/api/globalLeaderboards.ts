6fdadd6b4ac9d2c74cf040e57acecc9bb369fe19
Luciano Ratamero <luciano@ratamero.com>
2023-11-19 21:55:36 -0300

    Fix prettier configs

--- src/lib/api/globalLeaderboards.ts
+++ src/lib/api/globalLeaderboards.ts
@@ -1,25 +1,26 @@
-  import { PUBLIC_API_URL } from '$env/static/public';
-  import { createQuery } from '@tanstack/svelte-query';
+import { PUBLIC_API_URL } from '$env/static/public';
+import { createQuery } from '@tanstack/svelte-query';
 
-  type LeaderboardResponse = {
-    count: number;
-    next: string | null;
-    previous: string | null;
-    results: {
-      'id': number,
-      'username': string,
-      'tt': number,
-      'rank': number,
-      'country': string,
-      'picture': string | null,
-    }[];
-  }
+type LeaderboardResponse = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: {
+    id: number;
+    username: string;
+    tt: number;
+    rank: number;
+    country: string;
+    picture: string | null;
+  }[];
+};
 
-  const fetchLeaderboards = async ({ pageParam = 1 }) => {
-    return fetch(`${PUBLIC_API_URL}/globalleaderboard/?page=${pageParam}`).then(res => res.json());
-  };
+const fetchLeaderboards = async ({ pageParam = 1 }) => {
+  return fetch(`${PUBLIC_API_URL}/globalleaderboard/?page=${pageParam}`).then((res) => res.json());
+};
 
-  export const globalLeaderboardsQuery =  ({ pageParam = 1 }) => createQuery<LeaderboardResponse>({
+export const globalLeaderboardsQuery = ({ pageParam = 1 }) =>
+  createQuery<LeaderboardResponse>({
     queryKey: ['leaderboards'],
     queryFn: () => fetchLeaderboards({ pageParam })
-  })
+  });
