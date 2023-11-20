84101c61365253ffaa932b2bd73650fe60a176a3
Luciano Ratamero <luciano@ratamero.com>
2023-11-19 21:52:40 -0300

    Add fuctional sketch of the global leaderboards page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { globalLeaderboardsQuery } from '$lib/api/globalLeaderboards';
+
+  let pageParam = 1;
+
+  $: query = globalLeaderboardsQuery({ pageParam });
+  $: pageCount = $query.data ? $query.data.count / 50 : 1;
+</script>
+
+<h1>Leaderboards</h1>
+
+<button on:click={() => pageParam -= 1} disabled="{!$query.data?.previous}">&lt;</button>
+{#each Array.from({ length: pageCount }) as _, i}
+  <button on:click={() => pageParam = i + 1}>{i + 1}</button>
+{/each}
+<button on:click={() => pageParam += 1} disabled="{!$query.data?.next}">&gt;</button>
+
+<p>Page count: {pageCount}</p>
+{#if $query.isLoading}
+  <p>Loading...</p>
+{:else if $query.isError}
+  <p>Error: {$query.error.message}</p>
+{:else if $query.isSuccess}
+  {JSON.stringify($query.data.results)}
+{/if}
+
