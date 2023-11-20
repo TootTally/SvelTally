19ec121ad009677820de2a5133b0b5a42ca77051
Luciano Ratamero <luciano@ratamero.com>
2023-11-20 02:09:28 -0300

    Add color scheme to tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'toot-blue': '#00e6e4',
+        'toot-orange': '#f5a506',
+        'toot-yellow': '#f8e71c',
+        'toot-red': '#e13453',
+        'toot-pale-red': '#e05e75'
+      },
+      fontFamily: {
+        sans: ['Noto Sans', 'sans-serif'],
+        title: ['Multicolore']
+      }
+    }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
