ce13bf8df20360d2e7304cd4e5ae357e586869f5
Luciano Ratamero <luciano@ratamero.com>
2023-11-23 15:07:34 -0300

    Refactor Hero component styling in Hero.svelte
    The Hero component's styling has been significantly adjusted to streamline and enhance the look of the title. The changes included removal of unnecessary borders, the addition of a "bg" span, and adjusting padding and positioning for different screen sizes.

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,18 +3,15 @@
 </script>
 
 <div class="flex bg-gradient-to-br from-blue-600 to-pink-700 py-16">
-  <div class="m-auto flex w-full md:w-[80vw]">
-    <div
-      class="border border-b-[4.4em] border-l-[1em] border-t-0 border-blue-900 border-l-transparent border-opacity-50 sm:ml-4"
-    ></div>
+  <div
+    class="relative z-0 m-auto flex w-full justify-center overflow-hidden md:w-[80vw] md:justify-start"
+  >
     <h1
-      class="z-10 inline-block bg-blue-900 bg-opacity-50 px-3 pb-2 pt-4 font-title text-5xl leading-none text-black"
+      class="relative z-10 inline-block px-5 pb-2 pt-4 text-center font-title text-5xl leading-none text-black md:ml-5"
     >
+      <span class="bg" />
       {title}
     </h1>
-    <div
-      class="border border-b-0 border-r-[1em] border-t-[4.4em] border-blue-900 border-r-transparent border-opacity-50"
-    ></div>
   </div>
 </div>
 
@@ -30,5 +27,12 @@
       2px -2px #fff,
       -2px 2px #fff;
     z-index: 10;
+  }
+
+  .bg {
+    @apply absolute left-0 top-0 h-full w-full bg-blue-900 bg-opacity-50;
+    z-index: -1;
+    height: 100%;
+    transform: skewX(-30deg);
   }
 </style>
