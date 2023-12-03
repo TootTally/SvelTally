6ee2cc743f18b56effc686cae22e16939f37032e
Luciano Ratamero <luciano@ratamero.com>
2023-12-02 22:43:14 -0300

    Add switch language functionality to layout.svelte
    Included an event listener in layout.svelte for switching languages. Imported new methods from $paraglide/runtime and contained all logic for language changing in onSetLanguageTag callback function. The language preference is now also stored locally and preferred language is determined by checking local storage first, then falling back to URL parameter or source language.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,31 @@
   import NavBar from '$lib/components/NavBar.svelte';
   import { browser } from '$app/environment';
   import { page } from '$app/stores';
-  import { setLanguageTag, sourceLanguageTag, type AvailableLanguageTag } from '$paraglide/runtime';
+  import {
+    setLanguageTag,
+    sourceLanguageTag,
+    onSetLanguageTag,
+    type AvailableLanguageTag
+  } from '$paraglide/runtime';
   import ParaglideMetaTags from '$lib/i18n/ParaglideMetaTags.svelte';
+  import { goto } from '$app/navigation';
+  import { writable } from 'svelte/store';
 
   const queryClient = new QueryClient();
+  const preferredLanguage = writable(localStorage.getItem('lang') as AvailableLanguageTag | null);
+
+  onSetLanguageTag((newLanguageTag) => {
+    localStorage.setItem('lang', newLanguageTag);
+    $preferredLanguage = newLanguageTag;
+    if ($page.params.lang !== newLanguageTag) {
+      goto(`/${newLanguageTag}${$page.url.pathname.slice(3)}`);
+    }
+  });
 
   //Determine the current language from the URL. Fall back to the source language if none is specified.
-  $: lang = ($page.params.lang as AvailableLanguageTag) ?? sourceLanguageTag;
+  $: lang = $preferredLanguage
+    ? $preferredLanguage
+    : ($page.params.lang as AvailableLanguageTag) ?? sourceLanguageTag;
 
   //Set the language tag in the Paraglide runtime.
   //This determines which language the strings are translated to.
